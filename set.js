5a65295524fa2b5ce690537f13232ca61d5b4d66
anjana-04 <anjanarajasekhar041196@gmail.com>
2019-07-30 16:24:43 +0530

    ihi

--- set.js
+++ set.js
@@ -2,19 +2,28 @@ $(document).ready(function(){
     $("#get").click(function(){
         $.ajax({
             type:"GET",
-            url:"dada.json",
+            url:"http://jsonplaceholder.typicode.com/users",
             success:function(data){
 
-var output="<table class='table'><thead><tr><th>NAME</th><th>DOB</th><th>COLLEGE</th><th>MOB</th></tr></thead>" 
+
+var output=""
 for(var i in data){
-    output+="<tr><td>"+data[i].name+"</td>"
-    output+="<td>"+data[i].dob+"</td>"
-    output+="<td>"+data[i].college+"</td>"
-     output+="<td>"+data[i].mob+"</td></tr>"
+
+   output+="<div class='card'><div class='card-header'>  <div class='card-body'></div>"
+
+
+  output+="<tr><td>"+data[i].id+"</td>"
+    output+="<td>"+data[i].name+"</td>"
+    output+="<td>"+data[i].username+"</td>"
+     output+="<td>"+data[i].address.street+"</td>"
+     output+="<td>"+data[i].address.suite+"</td>"
+     output+="<td>"+data[i].address.city+"</td></tr>"
+     output+="</div>";
+
+
 }
-output+="</table>";
 $(".result").html(output);
-            }
+}
         })
     })
 })
